66be04e535b7930814a37854b0356fd007727e1e
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-02-19 13:40:20 -0800

    added a file for converting between default midi information and midibert format

---
+++ src/main/data/converter.py
@@ -0,0 +1,13 @@
+from typing import List, Tuple
+
+import numpy as np
+
+
+def convert_to_midibert(midi_sequences: List[np.ndarray]) -> np.ndarray:
+    """
+    Converts the preprocessed MIDI files into the compound words (CPs) in the
+    same format as the MidiBERT pre-training data.
+    :param midi_sequences: a list of MIDI sequence tuples
+    :return: a padded dataset of
+    """
+    pass
