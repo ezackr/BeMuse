f9577e398b7db7cbd567d8159dfe91f3184a6e5c
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-02-20 12:54:24 -0800

    added padding token to top-level fields

--- src/main/data/converter.py
+++ src/main/data/converter.py
@@ -1,6 +1,12 @@
-from typing import List, Tuple
+from typing import List
 
 import numpy as np
+
+BAR_PAD_TOKEN: int = 2
+POSITION_PAD_TOKEN: int = 16
+PITCH_PAD_TOKEN: int = 86
+DURATION_PAD_TOKEN: int = 64
+padding_word = [BAR_PAD_TOKEN, POSITION_PAD_TOKEN, PITCH_PAD_TOKEN, DURATION_PAD_TOKEN]
 
 
 def convert_to_midibert(midi_sequences: List[np.ndarray]) -> np.ndarray:
