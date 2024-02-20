5ef7d0f0af45271f9ec6afbd2a4f59810e8eb99c
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-02-20 14:17:38 -0800

    outlined method skeleton for duration retrieval

--- src/main/data/preprocess.py
+++ src/main/data/preprocess.py
@@ -13,7 +13,8 @@ PITCH_PAD_TOKEN: int = 86
 DURATION_PAD_TOKEN: int = 64
 padding_word = [BAR_PAD_TOKEN, POSITION_PAD_TOKEN, PITCH_PAD_TOKEN, DURATION_PAD_TOKEN]
 
-NUM_SUB_BEATS: int = 16
+NUM_POSITION_SUB_BEATS: int = 16
+NUM_DURATION_SUB_BEATS: int = 64
 
 
 def _time_signature_has_changed(time_sig_changes: List[TimeSignature], current_time: float) -> bool:
@@ -65,7 +66,7 @@ def get_bar_of_tick(current_tick: float, bar_to_ticks: List[float]) -> int:
     return len(bar_to_ticks)
 
 
-def get_position_of_tick(current_tick: float, bar_number: int, bar_to_ticks: List[float]) -> float:
+def get_position_of_tick(current_tick: float, bar_number: int, bar_to_ticks: List[float]) -> int:
     """
     Gets the position of a given tick value relative to the start of its bar.
     :param current_tick: a given tick value
@@ -75,11 +76,13 @@ def get_position_of_tick(current_tick: float, bar_number: int, bar_to_ticks: Lis
     """
     bar_length = bar_to_ticks[bar_number] - bar_to_ticks[bar_number - 1]
     abs_pos = (current_tick - bar_to_ticks[bar_number - 1]) / bar_length
-    return math.floor(abs_pos * NUM_SUB_BEATS)
+    return math.floor(abs_pos * NUM_POSITION_SUB_BEATS)
 
 
-def get_duration_of_note(tick_length: float, bar_number: int, bar_to_ticks: List[float]) -> float:
-    pass
+def get_duration_of_note(start_tick: float, end_tick: float, bar_to_ticks: List[float]) -> int:
+    start_bar = get_bar_of_tick(start_tick, bar_to_ticks)
+    end_bar = get_bar_of_tick(end_tick, bar_to_ticks)
+    return -1
 
 
 def _classify_bar(bar_number: int, words: List[Tuple[int, float, int, float]]) -> int:
@@ -107,10 +110,10 @@ def midi_to_tuple(file_path) -> List[Tuple[int, float, int, float]]:
     words = []
     for note in midi_data.instruments[0].notes:
         note_start_tick = midi_data.time_to_tick(note.start)
-        note_duration_tick = midi_data.time_to_tick(note.end - note.start)
+        note_end_tick = midi_data.time_to_tick(note.end)
         bar_number = get_bar_of_tick(note_start_tick, bar_to_ticks)
         position = get_position_of_tick(note_start_tick, bar_number, bar_to_ticks)
-        duration = get_duration_of_note(note_duration_tick, bar_number, bar_to_ticks)
+        duration = get_duration_of_note(note_start_tick, note_end_tick, bar_to_ticks)
         word = (_classify_bar(bar_number, words), position, note.pitch, duration)
         words.append(word)
     return words
