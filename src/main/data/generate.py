d87b6ce4d37b10a29ba4423995892e1d0cecb405
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-02-20 15:58:43 -0800

    implemented end-to-end dataset generation

--- src/main/data/generate.py
+++ src/main/data/generate.py
@@ -1,11 +1,37 @@
-import numpy as np
+from os.path import join
+from typing import List
 
+import numpy as np
+from tqdm import tqdm
+
+from src.main.data import get_random_transposition, pad, preprocess_midi
 from src.main.util import root_dir
 
 
-def generate_dataset() -> np.ndarray:
-    # preprocess data in given directory
-    # augment
-    # pad
+def _augment_dataset(midi_sequences: List[np.ndarray]) -> List[np.ndarray]:
+    """
 
-    pass
+    :param midi_sequences:
+    :return:
+    """
+    augmented_sequences = []
+    for sequence in tqdm(midi_sequences):
+        augmented_sequences.append(sequence)
+        augmented_sequences.append(get_random_transposition(sequence))
+    return augmented_sequences
+
+
+def generate_dataset(split_name: str = "train") -> np.ndarray:
+    """
+
+    :param split_name:
+    :return:
+    """
+    midi_dir = join(root_dir, "dataset", "mono-midi-transposition-dataset", "midi_files", split_name, "midi")
+    print(f"Loading data from path ${midi_dir}.")
+    midi_sequences = preprocess_midi(midi_dir)
+    print(f"Augmenting dataset.")
+    aug_midi_sequences = _augment_dataset(midi_sequences)
+    print(f"Padding dataset.")
+    padded_sequences = pad(aug_midi_sequences)
+    return padded_sequences
