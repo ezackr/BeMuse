2d30bd17b687e728f619263682d8c5d82c2e5ab9
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-02-20 19:52:11 -0800

    added split_to_length to data module

--- src/main/data/__init__.py
+++ src/main/data/__init__.py
@@ -1,2 +1,2 @@
-from src.main.data.augment import get_random_transposition
+from src.main.data.augment import get_random_transposition, split_to_length
 from src.main.data.preprocess import get_bar_of_tick, get_position_of_tick, midi_to_tuple, preprocess_midi, pad
